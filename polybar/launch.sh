9db3e445ed4d0748a663964cac03d9f876d0824b
José Santos <59588636+gorkawien@users.noreply.github.com>
2021-02-11 19:27:14 +0100

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+desktop=$(echo $DESKTOP_SESSION)
+count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+
+
+case $desktop in
+
+  
+
+    bspwm|/usr/share/xsessions/bspwm)
+    if type "xrandr" > /dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config_top_border &
+      done
+    else
+    polybar --reload mainbar-bspwm -c ~/.config/polybar/config_top_border &
+    fi
+    # second polybar at bottom
+    # if type "xrandr" > /dev/null; then
+    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #     MONITOR=$m polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config &
+    #   done
+    # else
+    # polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config &
+    # fi
+    ;;
+    
+esac
